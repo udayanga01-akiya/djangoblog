f29d86c076bed5ce61b1e2aca69a55c323258615
akila <udayangaakila693@gmail.com>
2026-09-11 14:53:06 +0900

    css,base.html 2026-09-11

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'blog/home.html', {'title': 'This is the Djangoblog Homepage.'})
+
+def about(request):
+    return render(request, 'blog/about.html', {'content': 'This is the Djangoblog team.'})
+def contact(request):
+    return render(request, 'blog/contact.html')
