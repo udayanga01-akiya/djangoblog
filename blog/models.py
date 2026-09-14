15f8ed8a2fa8ead3a17594f3b93491987b2f84f1
akila <udayangaakila693@gmail.com>
2026-09-14 14:33:24 +0900

    Add auto slug generation using slugify in Post model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='published')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
